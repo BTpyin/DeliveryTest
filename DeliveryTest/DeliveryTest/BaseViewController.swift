30043cef491f0d0dfa24244202bdf0eda50d2926
Bowie Tso <>
2022-01-06 10:33:25 +0800

    project init + git ignore

---
+++ DeliveryTest/DeliveryTest/BaseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeliveryTest
+//
+//  Created by Bowie Tso on 5/1/2022.
+//
+
+import UIKit
+
+class BaseViewController : UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DeliveryTest/DeliveryTest/RootNavigationController.swift
@@ -0,0 +1,33 @@
+//
+//  RootNavigationController.swift
+//  DeliveryTest
+//
+//  Created by Bowie Tso on 5/1/2022.
+//
+
+import UIKit
+
+class RootNavigationController: UINavigationController {
+
+//    var disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.backButtonTitle = ""
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    func showAlert(_ title: String?, okClicked: ((UIAlertAction) -> Void)? = nil) {
+      let alertVC = UIAlertController.init(title: title, message: nil, preferredStyle: .alert)
+      alertVC.addAction(UIAlertAction.init(title: "OK", style: .default, handler: okClicked))
+      present(alertVC, animated: true, completion: nil)
+    }
+    
+
+}
